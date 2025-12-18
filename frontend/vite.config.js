16fa4f3d869737951d9bdcfdc4702d54b98477b6
Rahatuzzaman31 <rahatuzzaman100@gmail.com>
2025-12-18 13:18:47 +0600

    Update page speed 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     cssCodeSplit: true,
-    cssMinify: 'lightningcss',
     minify: 'terser',
     terserOptions: {
       compress: {
